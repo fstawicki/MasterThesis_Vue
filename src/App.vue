4430525746e0a1b381f35f1d527ff9cd19052df9
fstawicki <stawickifilip1@gmail.com>
2022-12-17 23:15:32 +0100

    add DOM elements and styling

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,136 @@
+
+
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="logo" />
+    <h1 class="title">Vue Testing App</h1>
+    <div class="render_div">
+      <button class="btn">Render 1000</button>
+      <button class="btn">Render 10000</button>
+      <button class="btn">Render 25000</button>
+      <button class="btn">Render 50000</button>
+    </div>
+        <button class="btn">Copy Array</button>
+        <button class="btn">Edit Elements</button>
+        <button class="btn">Delete Elements</button>
+    <div class="line" />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+
+
+<script>
+  export default {
+    name: 'App',
+    data(){
+      return {
+        array1: []
+      }
+    }
   }
-}
 </script>
 
 <style lang="scss">
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
 }
+
+body{
+  background-color: #344b5c;
+}
+
+.container{
+  width: 35%;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+
+  background-color: #3c676a;
+}
+
+.logo{
+  background-image: url('./assets/logo.png');
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 200px;
+}
+
+.title{
+  color: #41b883;
+  margin: 1rem 0;
+  text-align: center;
+}
+
+.render_div{
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  
+  .btn{
+    width: 24%;
+  }
+}
+
+.line{
+  margin-top: 1.5rem;
+  height: 1px;
+  width: 100%;
+  background-color: white;
+}
+
+ul{
+  list-style: none;
+  text-align: left;
+  
+  margin: 1rem 0;
+
+  li{
+    background-color: #2596be;
+    font-size: 0.85rem;
+    text-align: center;
+    color: white;
+    padding: 1rem;
+    margin: 0.5rem 0;
+  }
+}
+
+.btn{
+  width: 100%;
+  margin-top: 1rem;
+  padding: 1rem 0;
+  background-color: #41b883;
+  color: white;
+  border: none;
+  cursor: pointer;
+  transition: 0.2s ease;
+
+  &:hover{
+    background-color: #349469;
+  }
+  
+}
+
 </style>
